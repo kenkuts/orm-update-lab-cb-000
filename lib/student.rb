4bebf01692300aca86dbaece06babdaddde61f88
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-08-16 20:44:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     SQL
     binding.pry
     DB[:conn].execute(sql, self.name, self.grade, self.id)
-    data = DB[:conn].execute("SELECT * FROM students WHERE id = (?)", self.id ).flatten)
+    data = DB[:conn].execute("SELECT * FROM students WHERE id = (?)", self.id ).flatten
 
   end
 
